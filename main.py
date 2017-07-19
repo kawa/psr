b0bcc67da2becd3a04e1123ee311dc8501364833
Kawa <tk@kawa.tk>
2017-07-19 13:40:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import RPi.GPIO as GPIO
  
 GPIO.setmode(GPIO.BCM)
 button_pin = 21
-GPIO.setup(button_pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button_pin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
  
 sw_status = 1
  
 while True:
     sw_status = GPIO.input(button_pin)
-    if sw_status == 0:
-        print("OK!")
-        break
+#    if sw_status == 0:
+#        print("OK!")
+#        break
+    print(sw_status)
     time.sleep(0.03)
  
 GPIO.cleanup()
