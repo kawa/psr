61a76a965358a7e3aa2cc3235067a827d1c7575c
khirakawa <ken.hirakawa@sony.com>
2017-07-20 19:05:00 +0900

    Test

--- test.py
+++ test.py
@@ -4,9 +4,8 @@ import signal
 import subprocess
 from subprocess import Popen
 
-startupinfo = subprocess.STARTUPINFO()
-startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-process = Popen(["/opt/retropie/emulators/retroarch/bin/retroarch", "-L", "/opt/retropie/libretrocores/lr-pcsx-rearmed/libretro.so", "/home/pi/RetroPie/roms/psx/kurassyu_01.iso"], startupinfo=startupinfo)
+FNULL = open(os.devnull, 'w')
+process = Popen(["/opt/retropie/emulators/retroarch/bin/retroarch", "-L", "/opt/retropie/libretrocores/lr-pcsx-rearmed/libretro.so", "/home/pi/RetroPie/roms/psx/kurassyu_01.iso"], stdout=FNULL, stderr=subprocess.STDOUT)
 time.sleep(4)
 process.send_signal(signal.SIGINT)
 process.wait()
