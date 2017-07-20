ff8f48960eeae0ed863fc00305916e73dc1da9c0
khirakawa <ken.hirakawa@sony.com>
2017-07-20 17:53:11 +0900

    readers

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python 
-# coding:utf-8 
+#!/usr/bin/python
+# coding:utf-8
 import time
 import os
 import RPi.GPIO as GPIO
- 
+
 GPIO.setmode(GPIO.BCM)
 button_pin = 21
 led_red_pin = 20
@@ -12,7 +12,7 @@ led_green_pin = 16
 GPIO.setup(button_pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(led_red_pin,GPIO.OUT,initial=GPIO.LOW)
 GPIO.setup(led_green_pin,GPIO.OUT,initial=GPIO.LOW)
- 
+
 sw_status = 1
 
 os.system("setterm -cursor off")
@@ -25,14 +25,20 @@ while True:
     if sw_status == 1:
         break
     time.sleep(0.03)
- 
+
 GPIO.output(led_red_pin,GPIO.LOW)
 GPIO.output(led_green_pin,GPIO.HIGH)
 
 os.system("omxplayer --refresh ps.mp4")
-os.system("/opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/lr-pcsx-rearmed/libretro.so ~/RetroPie/roms/psx/kurassyu_01.iso")
+
+# These are blocking codes
+#os.system("/opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/lr-pcsx-rearmed/libretro.so ~/RetroPie/roms/psx/kurassyu_01.iso")
 #os.system("/opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/lr-pcsx-rearmed/libretro.so ~/RetroPie/roms/psx/ace2_01.iso")
 #os.system("/opt/retropie/emulators/retroarch/bin/retroarch --fullscreen -L /opt/retropie/libretrocores/lr-pcsx-rearmed/libretro.so ~/RetroPie/roms/psx/doko_01.iso")
+
+# This is asynchronous
+# process = Popen(["/opt/retropie/emulators/retroarch/bin/retroarch", "-L", "/opt/retropie/libretrocores/lr-pcsx-rearmed/libretro.so", "/home/pi/RetroPie/roms/psx/kurassyu_01.iso"])
+
 os.system("setterm -cursor on")
 
 GPIO.cleanup()
--- test.py
+++ test.py
@@ -4,5 +4,5 @@ from subprocess import Popen
 
 process = Popen(["/opt/retropie/emulators/retroarch/bin/retroarch", "-L", "/opt/retropie/libretrocores/lr-pcsx-rearmed/libretro.so", "/home/pi/RetroPie/roms/psx/kurassyu_01.iso"])
 time.sleep(4)
-proc.send_signal(signal.SIGINT)
-proc.wait()
+process.send_signal(signal.SIGINT)
+process.wait()
