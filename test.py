fdd2b2c94aa3c8e3c34f77147c5512865a7f2b20
khirakawa <ken.hirakawa@sony.com>
2017-07-20 17:15:56 +0900

    test

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import os
 import time
 from subprocess import Popen
 
-process = Popen("/opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/lr-pcsx-rearmed/libretro.so ~/RetroPie/roms/psx/kurassyu_01.iso")
+process = Popen(["/opt/retropie/emulators/retroarch/bin/retroarch", "-L", "/opt/retropie/libretrocores/lr-pcsx-rearmed/libretro.so", "/home/pi/RetroPie/roms/psx/kurassyu_01.iso"])
 time.sleep(4)
 p.terminate()
